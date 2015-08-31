84dfa784d53145509d3baf59c43afb6e359b762a
Sluggern <hello@bjorn-eric.com>
2015-08-31 17:54:46 +0200

    Scroll to buttons

--- result.php
+++ result.php
@@ -41,10 +41,37 @@ include(ROOT_DIR.'/includes/functions.php');
     </div>
 </div>
 
+<!-----------------------Mobild Nav---------------------------->
+<nav class="mobile-nav layout--line">
+    <div class="item">
+        <a href="#wave-1">
+            <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored-1">
+                1
+            </button>
+        </a>
+    </div>
+    <div class="item">
+        <a href="#wave-2">
+            <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored-2">
+                2
+            </button>
+        </a>
+    </div>
+    <div class="item">
+        <a href="#wave-3">
+            <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored-3">
+                3
+            </button>
+        </a>
+    </div>
+</nav>
+
+
+
 <div class="layout---line">
 
     <!------------------ WAVE 1 ---------------->
-    <article class="wave-1 layout---unit">
+    <article id="wave-1" class="wave-1 layout---unit">
         <div class="heading">
             <h3 class="type--heading-wave">Wave 1</h3>
         </div>
@@ -113,7 +140,7 @@ include(ROOT_DIR.'/includes/functions.php');
 
 <!------------------ WAVE 2 ---------------->
 
-<article class="wave-2 layout---unit">
+<article id="wave-2" class="wave-2 layout---unit">
 	<div class="heading">
 		<h3 class="type--heading-wave">Wave 2</h3>
 	</div>
@@ -180,7 +207,7 @@ include(ROOT_DIR.'/includes/functions.php');
 
 <!------------------ WAVE 3 ---------------->
 
-<article class="wave-3 layout---unit">
+<article id="wave-3" class="wave-3 layout---unit">
 	<div class="heading">
 		<h3 class="type--heading-wave">Wave 3</h3>
 	</div>
