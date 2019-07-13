b61ea043f9a5b0af1ce00c3944bf359b9c91b9d8
Björn-Eric Abrahamsson <hello@bjorn-eric.com>
2019-07-13 09:34:27 +0200

    Indentation

--- lib/functions.php
+++ lib/functions.php
@@ -2,14 +2,14 @@
 
 require './vendor/kint.phar';
 
-function calculateWave($weightRM, $waveNr, $perc1, $perc2, $perc3)
+function calculateWave($weightRM, $waveNr, $percentage1, $percentage2, $percentage3)
 {
-    $workPercentage   = unserialize($_COOKIE['531_user'])["harder"] ? 0.95 : 0.90;
-    $weightTM = $weightRM * $workPercentage;
+    $workPercentage = unserialize(htmlentities($_COOKIE['531_user']))['harder'] ? 0.95 : 0.90;
+    $weightTM       = $weightRM * $workPercentage;
 
-    $perc1dec = $perc1 / 100;
-    $perc2dec = $perc2 / 100;
-    $perc3dec = $perc3 / 100;
+    $percentage1dec = $percentage1 / 100;
+    $percentage2dec = $percentage2 / 100;
+    $percentage3dec = $percentage3 / 100;
 
     if ($waveNr == 1) {
         $set1 = 5;
@@ -30,9 +30,9 @@ function calculateWave($weightRM, $waveNr, $perc1, $perc2, $perc3)
         <span class="cell span-warmup"><?php echo 5 * floor($weightTM * 0.5 / 5); ?> * 5</span>
         <span class="cell span-warmup"><?php echo 5 * floor($weightTM * 0.6 / 5); ?> * 3</span>
         <!------------ Main Lifts ------------>
-        <span class="cell"><?php echo 2.5 * floor($weightTM * $perc1dec / 2.5) . ' * ' . $set1; ?></span>
-        <span class="cell"><?php echo 2.5 * floor($weightTM * $perc2dec / 2.5) . ' * ' . $set2; ?></span>
-        <span class="cell span-final-set"><?php echo 2.5 * floor($weightTM * $perc3dec / 2.5) . ' * ' . $set3 . '+'; ?></span>
+        <span class="cell"><?php echo 2.5 * floor($weightTM * $percentage1dec / 2.5) . ' * ' . $set1; ?></span>
+        <span class="cell"><?php echo 2.5 * floor($weightTM * $percentage2dec / 2.5) . ' * ' . $set2; ?></span>
+        <span class="cell span-final-set"><?php echo 2.5 * floor($weightTM * $percentage3dec / 2.5) . ' * ' . $set3 . '+'; ?></span>
     </div>
 
     <!-- Main lifts -->
@@ -41,25 +41,25 @@ function calculateWave($weightRM, $waveNr, $perc1, $perc2, $perc3)
             <h3 class="heading--smallcaps heading--smallcaps-main">
                 Main lift:
                 <span class="result--heading">
-                    <?php echo floor((($weightTM * $perc1dec) * $set1) + (($weightTM * $perc2dec) * $set2) + (($weightTM * $perc3dec) * $set3)); ?>kg
+                    <?php echo floor((($weightTM * $percentage1dec) * $set1) + (($weightTM * $percentage2dec) * $set2) + (($weightTM * $percentage3dec) * $set3)); ?>kg
                 </span>
             </h3>
             <h3 class="heading--smallcaps">
                 +1:
                 <span class="result--heading-plus">
-                    <?php echo floor((($weightTM * $perc1dec) * $set1) + (($weightTM * $perc2dec) * $set2) + (($weightTM * $perc3dec) * $set3) + (($weightTM * $perc3dec) * $set3)); ?>kg
+                    <?php echo floor((($weightTM * $percentage1dec) * $set1) + (($weightTM * $percentage2dec) * $set2) + (($weightTM * $percentage3dec) * $set3) + (($weightTM * $percentage3dec) * $set3)); ?>kg
                 </span>
             </h3>
             <h3 class="heading--smallcaps">
                 +2:
                 <span class="result--heading-plus">
-                    <?php echo floor((($weightTM * $perc1dec) * $set1) + (($weightTM * $perc2dec) * $set2) + (($weightTM * $perc3dec) * $set3) + (($weightTM * $perc3dec) * $set3) + (($weightTM * $perc3dec) * $set3)); ?>kg
+                    <?php echo floor((($weightTM * $percentage1dec) * $set1) + (($weightTM * $percentage2dec) * $set2) + (($weightTM * $percentage3dec) * $set3) + (($weightTM * $percentage3dec) * $set3) + (($weightTM * $percentage3dec) * $set3)); ?>kg
                 </span>
             </h3>
             <h3 class="heading--smallcaps">
                 +3:
                 <span class="result--heading-plus">
-                    <?php echo floor((($weightTM * $perc1dec) * $set1) + (($weightTM * $perc2dec) * $set2) + (($weightTM * $perc3dec) * $set3) + (($weightTM * $perc3dec) * $set3) + (($weightTM * $perc3dec) * $set3) + (($weightTM * $perc3dec) * $set3)); ?>kg
+                    <?php echo floor((($weightTM * $percentage1dec) * $set1) + (($weightTM * $percentage2dec) * $set2) + (($weightTM * $percentage3dec) * $set3) + (($weightTM * $percentage3dec) * $set3) + (($weightTM * $percentage3dec) * $set3) + (($weightTM * $percentage3dec) * $set3)); ?>kg
                 </span>
             </h3>
         </div>
