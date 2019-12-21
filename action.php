a565fe2fadb1b57e92117513209aaac7254d9909
Björn-Eric Abrahamsson <hello@bjorn-eric.com>
2019-12-21 20:18:57 +0100

    styling

--- action.php
+++ action.php
@@ -5,27 +5,32 @@ $Edmar = isset($_POST['Edmar']) ? 'Yes' : 'No';
 $harder = isset($_POST['harder']);
 
 $user_data = [
-    'name'     => htmlspecialchars($_POST['name']),
-    'press'    => (int)$_POST['press'],
+    'name' => htmlspecialchars($_POST['name']),
+    'press' => (int)$_POST['press'],
     'deadlift' => (int)$_POST['deadlift'],
-    'bench'    => (int)$_POST['bench'],
-    'squat'    => (int)$_POST['squat'],
-    'Edmar'    => $Edmar,
-    'harder'   => $harder,
+    'bench' => (int)$_POST['bench'],
+    'squat' => (int)$_POST['squat'],
+    'Edmar' => $Edmar,
+    'harder' => $harder,
 ];
 
 // Set cookie for one year
 $time = time();
 $user_data_serialized = serialize($user_data);
+
 if ($_SERVER['SERVER_NAME'] === 'localhost') {
     setcookie('531_user', $user_data_serialized, $time + 32140800, '/', false, false);
 } else {
-    setcookie('531_user', $user_data_serialized, $time + 32140800, '/', true, true);
+    $set = setcookie('531_user', 'content', 1);
+    if (!$set) {
+        throw new Exception('Could not set Cookie');
+    }
+    setcookie('531_user', $user_data_serialized, $time + 32140800, '/visa/531');
 }
 
 // Program-url-trix
 $address = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$path    = str_replace('/action.php', '', $address);
+$path = str_replace('/action.php', '', $address);
 $address = '//' . $path . '/program.php';
 
 // Relocate to program
